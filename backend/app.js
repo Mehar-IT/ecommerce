8d18acec3b5ae8fc6386e0d7691f07c6b9ed7fe6
Mehar-It <hamzatariquearain@gmail.com>
2022-11-12 12:14:29 -0800

    jwttoken added and user model

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
 const errorMiddleWare = require("./middleware/error");
 const notFoundError = require("./middleware/404");
 
 app.use(express.json());
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 app.use(notFoundError);
 app.use(errorMiddleWare);
 
---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require("../models/userModel");
+const ErrorHandler = require("../utils/errorhandlers");
+const asyncErrorHandler = require("../middleware/asyncErrorHandler");
+const sendToken = require("../utils/jwtToken");
+
+exports.registerUser = asyncErrorHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a simple id",
+      url: "profilepicUrl",
+    },
+  });
+
+  sendToken(user, 201, res);
+});
+
+exports.loginUser = asyncErrorHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email and password", 401));
+  }
+  const isPasswordMatch = await user.comparePassowrd(password);
+
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email and password", 401));
+  }
+  sendToken(user, 200, res);
+});
