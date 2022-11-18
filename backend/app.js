13e0f1c0a55afb2ab7f2a53a6d7de3c0d8ddb3a3
Mehar-It <hamzatariquearain@gmail.com>
2022-11-19 00:59:25 +0500

    backend completed almost

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 const errorMiddleWare = require("./middleware/error");
 const notFoundError = require("./middleware/404");
 const cookieParser = require("cookie-parser");
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 app.use(notFoundError);
 app.use(errorMiddleWare);
 
